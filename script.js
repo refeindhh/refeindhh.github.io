080e54cea5d76f41843dff914c3f5f0cd9321e05
refeindhh <refeindhprogrammer@gmail.com>
2025-01-22 14:57:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function sendMessage() {
     const message = document.getElementById('message').value;
 
 
-    const url = "https://api.whatsapp.com/send?phone=6289530602815&text=Halo%20Admin%2C%0ASaya%20*"+ name +"*%0AEmail%20Address%20*"+ emailAddress +"*%0ANomor%20*"+ number +"*%0AEmail%20Subject%20*"+ emailSubject +"*%0A%0A*"+ message +"*%0A"
+    const url = "https://api.whatsapp.com/send?phone=6289530602815&text=Halo%20Admin%2C%0ASaya :%20*"+ name +"*%0AEmail%20Address :%20*"+ emailAddress +"*%0ANomor :%20*"+ number +"*%0AEmail%20Subject :%20*"+ emailSubject +"*%0A%0A*"+ message +"*%0A"
 
     window.open(url);
 }
